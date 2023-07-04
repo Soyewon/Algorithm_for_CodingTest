13fe863d5d5ff1133c35963e97dad5e075507f88
Yewon So <soyw96@gmail.com>
2023-07-04 18:53:43 +0900

    BOJ 18428/ dfs, Back tracking

---
+++ July_week1/BOJ18428.cpp
@@ -0,0 +1,97 @@
+// 백준 18428 - 감시 피하기
+// https://www.acmicpc.net/problem/18428
+// 백트래킹, 브루트포스
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+// 선생님 T
+// 학생 S
+// 장애물 O
+// 선생님은 대각선은 볼 수 x
+using namespace std;
+
+int n;
+char map[6][6];
+int dx[4] = {0, 0, -1, 1}; // 상하좌우 이동
+int dy[4] = {1, -1, 0, 0};
+
+bool flag; // 3개로 가능/불가능 여부
+vector<pair<int,int>> teacher;
+vector<pair<int,int>> corridor;
+
+bool avoidTeacher(int x, int y, int dir) {
+
+    while(1) {
+        x += dx[dir];
+        y += dy[dir];
+
+        if(map[x][y] == 'O' || x<0 || x>n || y<0 || y>n)
+            break;
+        // 길목에 학생이 있다면 선생님의 감시를 피하지 못한 것
+        if(map[x][y] == 'S')
+            return false;
+    }
+    return true; // 길목에 학생이 없다면 감시 피하기 성공
+}
+
+bool isPossible() {
+
+    for(int i=0; i<teacher.size(); i++) {
+        int x = teacher[i].first;
+        int y = teacher[i].second;
+
+        for(int dir=0; dir<4; dir++) { // 상하좌우 4방향 탐색
+            if(!avoidTeacher(x, y, dir)) // 감시를 피하지 못했다면
+                return false;
+        }
+    }
+    return true; // 감시를 피했다면
+}
+
+void dfs(int cnt) {
+
+    if(cnt == 3) {
+        if(isPossible()) // 감시를 피했을 때
+            flag = true;
+            return;
+    }
+
+    for(int i=0; i<corridor.size(); i++) {
+        int x = corridor[i].first;
+        int y = corridor[i].second;
+        if(map[x][y] == 'X') {
+            map[x][y] = 'O';
+            dfs(cnt+1);
+            map[x][y] = 'X';
+        }
+    }
+}
+
+int main() {
+
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n;
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            cin >> map[i][j];
+
+            if(map[i][j] == 'T')
+                teacher.push_back({i, j});
+            else if(map[i][j] == 'X')
+                corridor.push_back({i, j});
+        }
+    }
+
+    dfs(0);
+
+    if(flag)
+        cout << "YES";
+    else 
+        cout << "NO";
+
+    return 0;
+}
---
+++ June_week4/BOJ14500.cpp
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+
+using namespace std;
+
+
+int map[501][501];
+int visit[501][501];
+
+int dx[] = {-1, 0, 0, 1};
+int dy[] = {0, -1, 1, 0};
+
+int N, M, res=0;
+
+void GetMaxNum(int x, int y, int depth, int temp) {
+
+    if(depth == 4) { // 깊이가 4가 되면
+        if(res < temp) res = temp;
+        return;
+    }
+
+    for(int i=0; i<4; i++) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
+        if(nx < 1 || ny < 1 || nx > N || ny > M || visit[nx][ny]) 
+            continue;
+
+        visit[nx][ny] = 1;
+        GetMaxNum(nx, ny, depth+1, temp+map[nx][ny]);
+        visit[nx][ny] = 0; // 백트래킹
+    }
+
+    // ㅓ
+    if(x-1 >= 1 && y-1 >= 1 && x+1 <= N) {
+        res = max(res, (map[x-1][y] + map[x][y-1] + map[x][y] + map[x+1][y]));
+    }
+    // ㅏ
+    if(x-1 >= 1 && y+1 <= M && x+1 <= N) { 
+        res = max(res, (map[x-1][y] + map[x][y+1] + map[x][y] + map[x+1][y]));
+    }
+    // ㅗ
+    if(y-1 >= 1 && y+1 <= M && x+1 <= N) { 
+        res = max(res, (map[x][y] + map[x+1][y] + map[x+1][y-1] + map[x+1][y+1]));
+    }
+    // ㅜ
+    if(y-1 >= 1 && y+1 <= M && x+1 <= N) { 
+        res = max(res, (map[x][y-1] + map[x][y] + map[x][y+1] + map[x+1][y]));
+    }
+
+}
+
+int main() {
+
+    memset(visit, 0, sizeof(visit));
+
+    cin >> N >> M;
+    for(int i=1; i<=N; i++) { 
+        for(int j=1; j<=M; j++) {
+            cin >> map[i][j];
+        }
+    }
+
+    for(int i=1; i<=N; i++) {
+        for(int j=1; j<=M; j++) {
+            visit[i][j] = 1;
+            GetMaxNum(i, j, 1, map[i][j]);
+            visit[i][j] = 0; // 백트래킹
+        }
+    }
+    cout << res;
+}
+
